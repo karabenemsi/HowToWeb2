be5c5b4d1617bde66ee37b3d0f78cbe0641087bb
karabenemsi <karabenemsi.git@web.de>
2016-12-19 21:12:59 +0100

    StartPage Ready
    Ready to launch Preview

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -4,10 +4,37 @@ $( document ).ready(function() {
   $('.indexpage .js_openside').click(function(){
     var $parent = $(this).parent().parent();
     $parent.toggleClass('open');
+    $parent.siblings().toggleClass('closed');
     if ($parent.siblings().hasClass('open')) {
       $parent.siblings().toggleClass('open');
+    }if ($parent.hasClass('closed')) {
+      $parent.toggleClass('closed');
     }
   })
+
+
+
+  //nextslide
+  $('.nextslide').click(function(){
+    var id = $(this).parent().attr('id')
+    var $parent = $(this).parent()
+    id = parseInt(id) + 1
+    var $nextslide = $('#' + id)
+    console.log('#' + id);
+    $parent.toggleClass('active')
+    $nextslide.toggleClass('active')
+  });
+
+  $('.previouslide').click(function(){
+    var id = $(this).parent().attr('id')
+    var $parent = $(this).parent()
+    id = parseInt(id) - 1
+    var $nextslide = $('#' + id)
+    console.log('#' + id);
+    $parent.toggleClass('active')
+    $nextslide.toggleClass('active')
+
+  });
 
 
 });
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,9 @@
 //@prepros-append jquery-2.1.4.min.js
+//@prepros-append highlighter/shCore.js
+//@prepros-append highlighter/shAutoloader.js
+//@prepros-append highlighter/shLegacy.js
+//@prepros-append highlighter/shBrushCss.js
+//@prepros-append highlighter/shBrushXml.js
+//@prepros-append highlighter/shBrushJScript.js
 //@prepros-append functions.js
 //@prepros-append contextmenu.js
